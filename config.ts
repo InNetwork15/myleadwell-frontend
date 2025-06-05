1024ba7c365285e1354f9d3de7449bf89f8cde6c
Joshua Wright <josh@givemanagement.com>
2025-06-05 10:37:41 -0400

    Update frontend/backend

--- config.ts
+++ config.ts
@@ -2,5 +2,5 @@ import Constants from 'expo-constants';
 
 export const API_BASE_URL =
   Constants.expoConfig?.extra?.apiBaseUrl ??
-  process.env.EXPO_PUBLIC_API_BASE_URL ??
+  process.env.EXPO_PUBLIC_API_URL ??
   'https://myleadwell-backend.onrender.com';

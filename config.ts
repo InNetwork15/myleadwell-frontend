368bc7cfc7f4fdb9794eb9bdada886bbb9636c91
Joshua Wright <josh@givemanagement.com>
2025-06-04 20:35:08 -0400

    Update frontend/backend

--- config.ts
+++ config.ts
@@ -1,2 +1,7 @@
 import Constants from "expo-constants";
-export const apiBaseUrl = Constants.expoConfig?.extra?.apiBaseUrl ?? "https://www.myleadwell.com/api";
+
+// This prioritizes Expo Config, then environment variable, then default fallback
+export const apiBaseUrl =
+  Constants.expoConfig?.extra?.apiBaseUrl ||
+  process.env.EXPO_PUBLIC_API_BASE_URL ||
+  "https://myleadwell.onrender.com";
--- utils/config.ts
+++ utils/config.ts
@@ -1,1 +1,7 @@
-export const apiBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL;
+import Constants from "expo-constants";
+
+// This prioritizes Expo Config, then environment variable, then default fallback
+export const apiBaseUrl =
+  Constants.expoConfig?.extra?.apiBaseUrl ||
+  process.env.EXPO_PUBLIC_API_BASE_URL ||
+  "https://myleadwell.onrender.com";

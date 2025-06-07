a99fb2dab7d1417b9d31e4df100a660d835d5abc
Joshua Wright <josh@givemanagement.com>
2025-06-07 15:45:04 -0400

    Update frontend/backend

--- utils/config.ts
+++ utils/config.ts
@@ -1,5 +1,4 @@
 import Constants from "expo-constants";
 
-export const apiBaseUrl =
-  Constants.expoConfig?.extra?.apiBaseUrl ??
-  "https://myleadwell-backend.onrender.com";
+export const API_BASE_URL = 'https://myleadwell-backend.onrender.com';
+
